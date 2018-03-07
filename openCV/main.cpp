c10e854929a49a78205b90b8cac54365ca9b5564
lustigepepe <lustigepepe@web.de>
2018-03-07 14:41:57 +0100

    Initial Commit

---
+++ openCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openCV
+//
+//  Created by soWhat on 07.03.18.
+//  Copyright © 2018 soWhat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
