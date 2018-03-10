17b37800f35e0341ffd76a34feec221f044a8f84
lustigepepe <lustigepepe@web.de>
2018-03-10 17:08:27 +0100

    changed design

---
+++ ReadPic/readFishPic.hpp
@@ -0,0 +1,21 @@
+//
+//  recognizeFish.hpp
+//  openCV
+//
+//  Created by soWhat on 08.03.18.
+//  Copyright © 2018 soWhat. All rights reserved.
+//
+
+#ifndef recognizeFish_hpp
+#define recognizeFish_hpp
+
+#include <stdio.h>
+
+class ReadFishPic
+{
+public:
+    void ReadImgAndShow(int argc, char** argv);
+};
+
+
+#endif /* recognizeFish_hpp */
